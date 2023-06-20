0560c7675307b47ab361fd7b8639458147b6addb
Juan silva <juansinho98@gmail.com>
2023-06-19 23:27:27 -0300

    criando um arquivo js com informações do git hub e fazendo o update da branch main para producao

---
+++ github.js
@@ -0,0 +1,10 @@
+/*
+GIT CLONE "url do github" > Clonar arquivo do repositorio
+GIT ADD . > adicionar todos arquivos que tem no projeto
+GIT COMMIT -m "alteração de titulo" > Adicionar uma mensagem para salvar 
+GIT PUSH > enviar as alterações feitas localmente 
+GIT BRANCH > Podemos dizer que seria Main "produção" e Developer "homologação"
+GIT PULL > Atualizar o GIT caso apareça nova branch
+GIT CHECKOUT developer > acessar a branch que foi incrementada 
+GIT MERGE > Trazer de outra branch Ex: trazer de hcp para PRD seria: git merge hcp | GIT PUS "lembrando que voce precisa está no checkout da branch prd
+*/
